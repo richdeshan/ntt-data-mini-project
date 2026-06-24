ff9451a5c9b0caeff830260f8a6c795513516c30
richdeshan <richdeshan@gmail.com>
2026-06-24 23:27:26 +0700

    fix routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { router } from "@/routes";
 import { Toaster } from "./components/ui/sonner.tsx";
 
