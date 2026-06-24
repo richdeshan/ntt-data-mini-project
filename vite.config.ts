8f45742b86db9311802c4418ba8134340dc2efc4
richdeshan <richdeshan@gmail.com>
2026-06-25 00:02:06 +0700

    fix: change path/win32 to cross-platform node:path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path/win32'
+import path from 'node:path'
 
 export default defineConfig({
   plugins: [
